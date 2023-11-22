7c25a1e7bf76a58d2cabdc190257ed838ac80c37
Bard1en <greger.grym@gmail.com>
2023-11-22 10:20:36 +0100

    Add project files.

---
+++ Uppgift 5.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+ namespace uppgift5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tal = new int[3];
+            tal[0] = 2;
+            tal[1] = 3;
+            Console.WriteLine("Skriv in ditt tal till arrayen");
+            tal[2] = int.Parse(Console.ReadLine());
+            Console.WriteLine(tal[0] + " " + tal[1] + " " + tal[2]);
+        }
+    }
+}
